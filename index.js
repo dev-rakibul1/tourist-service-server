93d49f5a1988f42f15d16c5cf2b04402fe3a052d
dev-rakibul1 <fl.rakibul@gmail.com>
2022-11-08 12:09:39 +0600

    mongoDB sever is connected and services API added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -9,13 +10,35 @@ app.use(cors());
 app.use(express.json());
 
 // mongoDB connect
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8rcwewi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+  } finally {
+  }
+}
+run().catch((err) => console.log(err.message));
+
+// DB Connected
+const serviceCollection = client
+  .db("tourist-service-server")
+  .collection("tourist-data");
+console.log("Database connected");
+
+// service query
+app.get("/services", async (req, res) => {
+  try {
+    const cursor = serviceCollection.find({});
+    const result = await cursor.toArray();
+    res.send(result);
+  } finally {
+  }
 });
 
 app.get("/", (req, res) => {
