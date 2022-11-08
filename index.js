196534aa44fc25c646c0b8f0b3222cfed396fd09
dev-rakibul1 <fl.rakibul@gmail.com>
2022-11-08 11:17:49 +0600

    mongodb server is connected

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+// mongoDB connect
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8rcwewi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 app.get("/", (req, res) => {
   res.send("Hello world! Tourist server is running.");
 });
